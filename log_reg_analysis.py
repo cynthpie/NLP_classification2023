0767f895393332c81a7750499043842e75a86e52
cynthpie <134090009+cynthpie@users.noreply.github.com>
2023-11-11 11:02:19 +0000

    BOW files

--- log_reg_analysis.py
+++ log_reg_analysis.py
@@ -4,11 +4,11 @@ import pickle
 with open("dev_pred_lr_.pk", "rb") as target:
     dev_prd_lreg = pickle.load(target)
 
+with open("wrong_predictions_LR.pk","rb") as file:
+    wrong_prd_reg = pickle.load(file)
 print(len(dev_prd_lreg[(dev_prd_lreg["keyword"]=="homeless") & (dev_prd_lreg["label"]==True)]))
-wrong_homeless = (dev_prd_lreg[(dev_prd_lreg["keyword"]=="homeless") & (dev_prd_lreg["log_reg_prd"]!=dev_prd_lreg["label"])])
-correct_true_homeless = dev_prd_lreg[(dev_prd_lreg["keyword"]=="homeless") & (dev_prd_lreg["label"]==False) & (dev_prd_lreg["log_reg_prd"]==False)]
-print(wrong_homeless)
-
-# wrong example
-print(wrong_homeless["text"].iloc[1])
+wrong_disabled = (dev_prd_lreg[(dev_prd_lreg["keyword"]=="homeless") & (dev_prd_lreg["log_reg_prd"]!=dev_prd_lreg["label"])])
+correct_true_disabled = dev_prd_lreg[(dev_prd_lreg["keyword"]=="homeless") & (dev_prd_lreg["label"]==False) & (dev_prd_lreg["log_reg_prd"]==False)]
+print(wrong_disabled)
+print(wrong_disabled["text"].iloc[1])
 
